d8382da2497e612e482e6d2da93a935a70b7e65c
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-29 20:05:32 +0200

    add a function to fetch values from the 'add book' form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -7,7 +7,37 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function getUserInput() {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const readStatus = document.querySelector('#checkbox').checked;
 
+  return {
+    title,
+    author,
+    pages,
+    readStatus,
+  };
 }
+
+function addBookToLibrary() {
+  // Declare a variable and assign it the value from getUserInput
+  const {
+    title, author, pages, readStatus,
+  } = getUserInput();
+  // Create a book instance
+  // const newBook = new Book(title, author, pages, readStatus);
+  // Push the new book to the myLibrary array
+  // myLibrary.push(newBook);
+
+  console.log(title);
+  console.log(author);
+  console.log(readStatus);
+}
+
+const addBook = document.querySelector('#addBook');
+
+addBook.addEventListener('click', () => {
+  addBookToLibrary();
+});
