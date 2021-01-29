c766244f59a84caa8962b54bb821ade518cbf5b5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-29 19:00:09 +0200

    Add book Object

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+
+}
