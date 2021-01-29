d0aa6d29d3fa6172ad5f7482fd4aca055dabee09
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-01-29 23:02:01 +0200

    Add and display the books

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readStatus = readStatus;
 }
 
 function getUserInput() {
@@ -25,7 +25,7 @@ function validateForm(obj) {
   const formValues = Object.values(obj);
   let emptyInputTagsCount = 0;
   for (let i = 0; i < formValues.length; i += 1) {
-    if (formValues[i].length === 0) emptyInputTagsCount += 1;
+	  if (formValues[i].length === 0) emptyInputTagsCount += 1;
   }
 
   if (emptyInputTagsCount > 0) return false;
@@ -38,15 +38,15 @@ function notifyUser(obj) {
   const pagesNotice = document.querySelector('#pages-notice');
 
   if (obj.title.length === 0) {
-    titleNotice.innerHTML = 'Title is required';
+	  titleNotice.innerHTML = 'Title is required';
   }
 
   if (obj.author.length === 0) {
-    authorNotice.innerHTML = 'Author is required';
+	  authorNotice.innerHTML = 'Author is required';
   }
 
   if (obj.pages.length === 0) {
-    pagesNotice.innerHTML = 'Number of pages is required';
+	  pagesNotice.innerHTML = 'Number of pages is required';
   }
 }
 
@@ -56,6 +56,32 @@ function cleanNoticeBoard() {
   document.querySelector('#pages-notice').innerHTML = '';
 }
 
+function addCard(obj){
+  const card = `<div class='col-sm-4 my-2'>
+    <div class='card text-center text-dark bg-light'>
+      <div class='card-header'>
+        ${obj.title}
+      </div>
+      <div class='card-body'>
+        <h5 class='card-title'>${obj.author}</h5>
+        <p class='card-text'>${obj.pages} pages</p>
+        ${obj.readStatus ? "<a href='#' class='btn btn-success'>Read</a>" : 
+        "<a href='#' class='btn btn-primary'>Not read</a>" }
+        <a href='#' class='btn btn-danger'>Delete</a>
+      </div>
+    </div>
+  </div>`;
+
+  return card;
+}
+
+function printCard(arr){
+  let markup = arr.map(elt => addCard(elt) ).join('');
+  let books_list = document.querySelector('#books_list');
+
+  books_list.innerHTML = markup;
+}
+
 function addBookToLibrary() {
   cleanNoticeBoard();
 
@@ -63,7 +89,7 @@ function addBookToLibrary() {
   if (!formIsValid) return notifyUser(getUserInput());
 
   const {
-    title, author, pages, readStatus,
+	  title, author, pages, readStatus,
   } = getUserInput();
   const newBook = new Book(title, author, pages, readStatus);
   myLibrary.push(newBook);
@@ -73,4 +99,5 @@ const addBook = document.querySelector('#addBook');
 
 addBook.addEventListener('click', () => {
   addBookToLibrary();
+  printCard(myLibrary);
 });
