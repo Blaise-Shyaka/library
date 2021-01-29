1fbe60d8c0a655d5163d93b80f167c455d0b0bf3
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-29 22:17:06 +0200

    add validation to the 'add book' form inputs

--- script.js
+++ script.js
@@ -21,19 +21,52 @@ function getUserInput() {
   };
 }
 
+function validateForm(obj) {
+  const formValues = Object.values(obj);
+  let emptyInputTagsCount = 0;
+  for (let i = 0; i < formValues.length; i += 1) {
+    if (formValues[i].length === 0) emptyInputTagsCount += 1;
+  }
+
+  if (emptyInputTagsCount > 0) return false;
+  return true;
+}
+
+function notifyUser(obj) {
+  const titleNotice = document.querySelector('#title-notice');
+  const authorNotice = document.querySelector('#author-notice');
+  const pagesNotice = document.querySelector('#pages-notice');
+
+  if (obj.title.length === 0) {
+    titleNotice.innerHTML = 'Title is required';
+  }
+
+  if (obj.author.length === 0) {
+    authorNotice.innerHTML = 'Author is required';
+  }
+
+  if (obj.pages.length === 0) {
+    pagesNotice.innerHTML = 'Number of pages is required';
+  }
+}
+
+function cleanNoticeBoard() {
+  document.querySelector('#title-notice').innerHTML = '';
+  document.querySelector('#author-notice').innerHTML = '';
+  document.querySelector('#pages-notice').innerHTML = '';
+}
+
 function addBookToLibrary() {
-  // Declare a variable and assign it the value from getUserInput
+  cleanNoticeBoard();
+
+  const formIsValid = validateForm(getUserInput());
+  if (!formIsValid) return notifyUser(getUserInput());
+
   const {
     title, author, pages, readStatus,
   } = getUserInput();
-  // Create a book instance
-  // const newBook = new Book(title, author, pages, readStatus);
-  // Push the new book to the myLibrary array
-  // myLibrary.push(newBook);
-
-  console.log(title);
-  console.log(author);
-  console.log(readStatus);
+  const newBook = new Book(title, author, pages, readStatus);
+  myLibrary.push(newBook);
 }
 
 const addBook = document.querySelector('#addBook');
